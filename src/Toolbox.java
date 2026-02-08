db013616e4b1b888efce123b611c767ae22138f0
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-02-08 00:54:30 -0800

    finished hasBalancedParentheses method. All tests are successful.

--- src/Toolbox.java
+++ src/Toolbox.java
@@ -1,6 +1,7 @@
 import java.util.Map;
 import java.util.Queue;
 import java.util.HashMap;
+import java.util.Stack;
 
 public class Toolbox {
 
@@ -277,7 +278,20 @@ public class Toolbox {
     if (input == null) {
       throw new IllegalArgumentException("Input string cannot be null.");
     }
-    return false;
+
+    Stack<Character> openings = new Stack<>();
+
+    for (char current : input.toCharArray()) {
+      if (current == '(') {
+        openings.push(current);
+      } else if (current == ')') {
+        if (openings.isEmpty()) {
+          return false;
+        }
+        openings.pop();
+      }
+    }
+    return openings.isEmpty();
   }
 
   /**
