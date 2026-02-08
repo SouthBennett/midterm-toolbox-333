583a464be455e93355a0e6673630351f69d2e0a2
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-02-07 23:44:10 -0800

    finished rotateQueueLeft method. All tests are successful

--- src/Toolbox.java
+++ src/Toolbox.java
@@ -251,7 +251,11 @@ public class Toolbox {
     if (queue == null || k < 0) {
       throw new IllegalArgumentException("Queue cannot be null and k cannot be negative.");
     }
-    
+
+    for (int i = 0; i < k; i++) {
+      int num = queue.remove();
+      queue.add(num);
+    }
   }
 
   /**
